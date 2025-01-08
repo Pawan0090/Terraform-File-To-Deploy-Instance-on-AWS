2d23b720de9dbe06668a9c50421cbc953bc2e195
Pawan0090 <pawrastogi0009@gmail.com>
2025-01-08 15:42:01 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "webserver" {
   ami = var.ami[var.OS]
   instance_type = var.instance
 
-  tags = {
-    Name = "My-Web-Server"
+  provisioner "local-exec" {
+        command = "echo ${self.private_ip} >> /Users/pawanrastogi/new.txt "
+  
+    }
+
+      count = 4
+tags = {
+  Name = "Worker-Node ${count.index+1}"
+}
+
+
+}
+
+  output "private_ip" {
+    value = aws_instance.webserver[*].private_ip
   }
+output "public_ip" {
+  value = aws_instance.webserver[*].public_ip
 }
