898c12eea3895a89985ac923bed1d71657fadc66
Pawan0090 <pawrastogi0009@gmail.com>
2025-01-08 15:43:02 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,17 @@
-variable "AWS_ACCESS_KEY" {
-    description = "Hi Please enter your AWS Access Key " 
-    sensitive = true
-}
-
-variable "AWS_SECRET_KEY" {
-  description = "Please Enter your Secret Key"
-  sensitive = true
-}
-
-variable "AWS_REGION" {
-  default = "us-east-1"
-}
-
 variable "OS" {
-  description = "Please Enter the OS of from (linux , ubuntu , debian)"
+  description = "Please Enter the OS of from (linux , ubuntu or 22.04 , debian)"
   #default = "ubuntu"
 }
 
 variable "instance" {
-  default = "t2.micro"
+  default = "t2.medium"
 }
 
 variable "ami" {
   default = {
-    linux="ami-01816d07b1128cd2d"
-    ubuntu="ami-0e2c8caa4b6378d8c"
-    debian="ami-064519b8c76274859"
+    linux  = "ami-01816d07b1128cd2d"
+    ubuntu = "ami-0e2c8caa4b6378d8c"
+    ubuntu22 = "ami-005fc0f236362e99f"
+    debian = "ami-064519b8c76274859"
   }
 }
